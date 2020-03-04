b81a95a21fd36bdc66c978b03f1c4a40dba5342a
TienTho98 <50016575+HoozukinoReitetsu@users.noreply.github.com>
2020-03-04 11:46:00 +0700

    fix admin template

--- admin/views/partial/footer.php
+++ admin/views/partial/footer.php
@@ -1,55 +1,36 @@
-<!--Footer-part-->
-
-<div class="row-fluid">
-  <div id="footer" class="span12"> 2013 &copy; Matrix Admin. Brought to you by <a href="http://themedesigner.in">Themedesigner.in</a> </div>
-</div>
-
-<!--end-Footer-part-->
-<script src="assets/js/excanvas.min.js"></script> 
-<script src="assets/js/jquery.min.js"></script> 
-<script src="assets/js/jquery.ui.custom.js"></script> 
-<script src="assets/js/bootstrap.min.js"></script> 
-<script src="assets/js/jquery.flot.min.js"></script> 
-<script src="assets/js/jquery.flot.resize.min.js"></script> 
-<script src="assets/js/jquery.peity.min.js"></script> 
-<script src="assets/js/fullcalendar.min.js"></script> 
-<script src="assets/js/matrix.js"></script> 
-<script src="assets/js/matrix.dashboard.js"></script> 
-<script src="assets/js/jquery.gritter.min.js"></script> 
-<script src="assets/js/matrix.interface.js"></script> 
-<script src="assets/js/matrix.chat.js"></script> 
-<script src="assets/js/jquery.validate.js"></script> 
-<script src="assets/js/matrix.form_validation.js"></script> 
-<script src="assets/js/jquery.wizard.js"></script> 
-<script src="assets/js/jquery.uniform.js"></script> 
-<script src="assets/js/select2.min.js"></script> 
-<script src="assets/js/matrix.popover.js"></script> 
-<script src="assets/js/jquery.dataTables.min.js"></script> 
-<script src="assets/js/matrix.tables.js"></script> 
-
-<script type="text/javascript">
-  // This function is called from the pop-up menus to transfer to
-  // a different page. Ignore if the value returned is a null string:
-  function goPage (newURL) {
-
-      // if url is empty, skip the menu dividers and reset the menu selection to default
-      if (newURL != "") {
-      
-          // if url is "-", it is this page -- reset the menu:
-          if (newURL == "-" ) {
-              resetMenu();            
-          } 
-          // else, send page to designated URL            
-          else {  
-            document.location.href = newURL;
-          }
-      }
-  }
-
-// resets the menu selection upon entry to this page:
-function resetMenu() {
-   document.gomenu.selector.selectedIndex = 2;
-}
-</script>
+  </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <!-- <script src="../../dist/js/pages/dashboards/dashboard1.js"></script> -->
+    <!-- Charts js Files -->
+    <script src="assets/libs/flot/excanvas.js"></script>
+    <script src="assets/libs/flot/jquery.flot.js"></script>
+    <script src="assets/libs/flot/jquery.flot.pie.js"></script>
+	<script src="assets/libs/flot/jquery.flot.time.js"></script>
+	<script src="assets/extra-libs/multicheck/jquery.multicheck.js"></script>
+    <script src="assets/libs/flot/jquery.flot.stack.js"></script>
+    <script src="assets/libs/flot/jquery.flot.crosshair.js"></script>
+	<script src="assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+	<script src="assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
+	<script src="dist/js/pages/chart/chart-page-init.js"></script>
+	<script src="assets/extra-libs/DataTables/datatables.min.js"></script>
 </body>
+
 </html>
